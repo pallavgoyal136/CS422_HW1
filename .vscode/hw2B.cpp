4ffa4b6a7e7a154436a96c25541168538e7dd520
Nipun Nohria <nipun22@csews30.cse.iitk.ac.in>
2025-03-21 21:32:14 +0530

    part B

--- .vscode/hw2B.cpp
+++ .vscode/hw2B.cpp
@@ -13,6 +13,15 @@ ofstream OutFile;
 #define gshare_gag_hybrid_height 512
 #define mask512 0x1FF
 #define mask1024 0x3FF
+#define mask128 0x7F
+#define masktag 0x3FFFFFF
+#define shifttag 32
+#define shiftvalid 62
+#define shiftlru 58
+#define masklru 0x3
+#define masktarget 0xFFFFFFFF
+#define numways 4
+#define numsets 128 
 bool FNBT;
 bool bimodal;
 bool sag;
@@ -31,6 +40,8 @@ INT8 gshare_pht[gshare_pht_height];
 INT8 sag_gag_hybrid_pht[sag_gag_hybrid_height];
 INT8 gshare_sag_hybrid_pht[gshare_sag_hybrid_height];
 INT8 gshare_gag_hybrid_pht[gshare_gag_hybrid_height];
+INT64 BTB_PC[numsets][numways];
+INT64 BTB_H[numsets][numways];
 UINT64 icount=0;
 UINT64 fast_forward_count;
 UINT64 forward_branches=0;
@@ -51,6 +62,11 @@ UINT64 backward_gshare=0;
 UINT64 backward_sag_gag_hybrid=0;
 UINT64 backward_sag_gag_gshare_hybrid_majority=0;
 UINT64 backward_sag_gag_gshare_hybrid_tournament=0;
+UINT64 miss_BTB_PC=0;
+UINT64 mispred_BTB_PC=0; 
+UINT64 miss_BTB_H=0;
+UINT64 mispred_BTB_H=0; 
+UINT64 control_flow=0;
 std::chrono::time_point<std::chrono::system_clock> startTime;
 std::chrono::time_point<std::chrono::system_clock> endTime;
 VOID docount(UINT32 c) { icount += c;}
@@ -62,6 +78,71 @@ ADDRINT CheckFastForward (void) {
 }
 ADDRINT FastForward (void) {
     return analyze;
+}
+VOID predict_control_flow_ins(ADDRINT pc, ADDRINT target)
+{
+    UINT32 index =  pc&mask128;
+    UINT32 tag = pc>>7;
+    UINT32 pred;
+    int hit=0, way=0;
+    UINT64 curr;
+    for(UINT i=0;i<4;i++)
+    {
+        if((BTB_PC[index][i]>>shifttag)&masktag == tag && BTB_PC[index][i]>>shiftvalid==1)
+        {
+            hit=1;
+            way=i;
+            curr=(BTB_PC[index][i]>>shiftlru)&masklru;
+        }
+    }
+    if(hit==0){
+        pred=pc+4;
+        miss_BTB_PC++;
+    }
+    else
+        pred=BTB_PC[index][way]&masktarget;
+    if(pred!=target)
+        mispred_BTB_PC++;
+    if(hit==0)
+    {
+        for(UINT i=0;i<4;i++)
+        {
+            if(BTB_PC[index][i]>>shiftvalid==0 || (BTB_PC[index][i]>>shiftlru)&masklru==3)
+            {
+                BTB_PC[index][i]=tag<<shifttag;
+                BTB_PC[index][i]=BTB_PC[index][i]|(1<<shiftvalid);
+                BTB_PC[index][i]=BTB_PC[index][i]|(target<<shifttarget);
+                way=i;
+                break;
+            }
+        }
+        for(UINT i=0;i<4;i++)
+        {
+            if(i!=way)
+            {
+                BTB_PC[index][i]=(((BTB_PC[index][i]>>shiftlru)+1)<<shiftlru)|(BTB_PC[index][i]&masklru);
+            }
+            else
+            {
+                BTB_PC[index][i]=(1<<shiftvalid)|(tag<<shifttag)|(target);
+            }
+        }
+    }
+    else
+    {
+        for(UINT i=0;i<4;i++)
+        {
+            if(i!=way && (BTB_PC[index][i]>>shiftlru)&masklru<curr)
+            {
+                BTB_PC[index][i]=(((BTB_PC[index][i]>>shiftlru)+1)<<shiftlru)|(BTB_PC[index][i]&masklru);
+            }
+            else if(i==way)
+            {
+                BTB_PC[index][i]=(1<<shiftvalid)|(tag<<shifttag)|(target);
+            }
+        }
+    }
+    control_flow++;
 }
 VOID predict_unconditional_branch(ADDRINT pc,ADDRINT target){
     FNBT=(pc>target);
@@ -196,6 +277,7 @@ void MyExitRoutine() {
     OutFile<<"SAg-GAg-GShare Hybrid Tournament : Accesses "<<total_branches<<", Mispredictions "<<forward_sag_gag_gshare_hybrid_tournament+backward_sag_gag_gshare_hybrid_tournament<<" ("<<(double)(forward_sag_gag_gshare_hybrid_tournament+backward_sag_gag_gshare_hybrid_tournament)/total_branches<<"), Forward branches "<<forward_branches<<", Forward mispredictions "<<forward_sag_gag_gshare_hybrid_tournament<<" ("<<(double)forward_sag_gag_gshare_hybrid_tournament/forward_branches<<"), Backward branches "<<backward_branches<<", Backward mispredictions "<<backward_sag_gag_gshare_hybrid_tournament<<" ("<<(double)backward_sag_gag_gshare_hybrid_tournament/backward_branches<<")\n";
     OutFile<<"\n";
     OutFile<<"Branch Target Predictors\n";
+    OutFile<<"BTB1 : Accesses "<<control_flow<<", Mispredictions "<<mispred_BTB_PC<<" ("<<(double)mispred_BTB_PC/control_flow<<"), Misses "<<miss_BTB_PC<<" ("<<(double)miss_BTB_PC/control_flow<<")\n";
     OutFile<<"===============================================\n";
     endTime = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = endTime - startTime;
@@ -216,6 +298,11 @@ VOID Trace(TRACE trace, VOID *v){
                 INS_InsertThenCall(ins,IPOINT_AFTER,(AFUNPTR)update_fall_through,IARG_ADDRINT,(ADDRINT)INS_Address(ins),IARG_END);
                 INS_InsertIfCall(ins,IPOINT_TAKEN_BRANCH, (AFUNPTR)FastForward, IARG_END);
                 INS_InsertThenCall(ins,IPOINT_TAKEN_BRANCH,(AFUNPTR)update_taken_branch,IARG_ADDRINT,(ADDRINT)INS_Address(ins),IARG_END);
+            }
+            if(INS_IsIndirectControlFlow(ins))
+            {
+                INS_InsertIfCall(ins,IPOINT_BEFORE, (AFUNPTR)FastForward, IARG_END);
+                INS_InsertThenCall(ins,IPOINT_BEFORE,(AFUNPTR)predict_control_flow_ins, IARG_ADDRINT,(ADDRINT)INS_Address(ins),IARG_BRANCH_TARGET_ADDR,IARG_END);
             }
         }
         BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)docount, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
@@ -248,6 +335,11 @@ int main(int argc, char * argv[])
     for(int i=0;i<sag_gag_hybrid_height;i++) sag_gag_hybrid_pht[i]=0;
     for(int i=0;i<gshare_sag_hybrid_height;i++) gshare_sag_hybrid_pht[i]=0;
     for(int i=0;i<gshare_gag_hybrid_height;i++) gshare_gag_hybrid_pht[i]=0;
+    for(int i=0;i<numsets;i++){
+        for(int j=0;j<numways;j++){
+            BTB_PC[i][j]=0;
+        }
+    }
     ghr=0;
     OutFile.open(KnobOutputFile.Value().c_str());
     fast_forward_count = KnobForwardCount.Value();
